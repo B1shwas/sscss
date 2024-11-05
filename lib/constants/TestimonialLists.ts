a1b1a5ee5189ff694e4deb6f607b76024fcc404e
Bishwash <bishwash312@gmail.com>
2024-11-05 19:51:09 +0545

    final

--- lib/constants/TestimonialLists.ts
+++ lib/constants/TestimonialLists.ts
@@ -1,38 +1,34 @@
-import testimony1 from "@/public/images/testimony/t1.jpg";
-import testimony2 from "@/public/images/testimony/t2.jpg";
-import testimony3 from "@/public/images/testimony/t5.jpg";
-import testimony4 from "@/public/images/testimony/t4.jpg";
-import testimony5 from "@/public/images/testimony/t3.jpg";
+import { bishwash, dhiraj, gelbu, lila, parent } from "../imports";
 
 export const TestimonialLists = [
   {
     name: "Bishwash Deoju",
     relation: "Student (2076)",
     text: '"My time at Saraswati Secondary School was transformative. The supportive teachers inspired me to pursue my passions, and the diverse activities helped me grow both academically and personally."',
-    image: testimony1,
+    image: bishwash,
   },
   {
     name: "Dhiraj Jirel",
     relation: "Student (2076)",
     text: '"Saraswati Secondary School provided me with an excellent foundation. The emphasis on critical thinking and creativity prepared me for higher education and beyond. I cherish the friendships I made there."',
-    image: testimony2,
+    image: dhiraj,
   },
   {
     name: "Pasang Sherpa",
     relation: "Student (2076)",
     text: '"The school’s vibrant community made learning enjoyable. I appreciated the balance of academics and extracurricular activities, which helped me develop leadership skills that I still use today."',
-    image: testimony3,
+    image: gelbu,
   },
   {
     name: "Lila Jirel",
     relation: "Parent",
     text: `"We couldn't be happier with our decision to enroll our daughter at Saraswati. The community is warm and welcoming, and the focus on holistic development prepared her well for her future."`,
-    image: testimony4,
+    image: lila,
   },
   {
     name: "Mr. Rai",
     relation: "Teacher",
     text: '"Saraswati Secondary School is a place where you can grow academically, develop leadership skills, and make meaningful connections. I look forward to the future at our school."',
-    image: testimony5,
+    image: parent,
   },
 ];
